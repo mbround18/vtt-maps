af02a0f00de6a42bdd05f141d6e5fc77f18ea22b
mbround18 <michael.bruno1337@gmail.com>
2024-10-04 14:03:46 -0700

    for testing purposes

--- packages/gh-pagify/src/pages/catalog.rs
+++ packages/gh-pagify/src/pages/catalog.rs
@@ -1,5 +1,9 @@
 use yew::{function_component, html, Html};
 
+#[cfg(test)]
+const CATALOG_CONTENT: &str = "<div>Default Catalog Content for Testing</div>";
+
+#[cfg(not(test))]
 const CATALOG_CONTENT: &str = include_str!("../../../../dist/assets/catalog.html");
 
 #[function_component(Catalog)]
